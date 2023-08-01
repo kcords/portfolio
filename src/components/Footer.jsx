1c087f76e73e99449c017d809cfa77398e1ca7a5
Kyle Cordell <kcordell-git@outlook.com>
2023-08-01 13:59:14 -0700

    Updates social icons behavior and positioning

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,105 +1,128 @@
 import React, { Component } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
 import Lottie from "react-lottie-player";
 import linkedInIcon from "../animations/icons8-linkedin.json";
 import gitHubIcon from "../animations/icons8-github.json";
+
 class Footer extends Component {
-  render() {
-    if (this.props.sharedBasicInfo) {
-      var networks = this.props.sharedBasicInfo.social.map(function (network) {
-        return (
-          <span key={network.name} className="m-4">
-            <a href={network.url} target="_blank" rel="noopener noreferrer">
-              <i className={network.class}></i>
-            </a>
-          </span>
-        );
-      });
+  constructor(props) {
+    super(props);
+    this.state = {
+      animationsActive: false,
+    };
+  }
+
+  componentDidMount() {
+    this.toggleValue();
+    this.interval = setInterval(this.toggleValue, 6000);
+  }
+
+  componentWillUnmount() {
+    clearInterval(this.interval);
+  }
+
+  toggleValue = () => {
+    this.setState((prevState) => ({
+      animationsActive: !prevState.animationsActive,
+    }));
+
+    if (!this.state.animationsActive) {
+      setTimeout(() => {
+        this.setState({ animationsActive: false });
+      }, 1120);
     }
+  };
+
+  render() {
+    const { animationsActive } = this.state;
 
     return (
-      <>
-        <div id="socials" className="socials">
-          <a
-            href={
-              this.props?.sharedBasicInfo?.social?.find(
-                (network) => network.name === "linkedin"
-              )?.url
-            }
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Lottie
-              loop
-              animationData={linkedInIcon}
-              play
-              className="socicon"
-            />
-          </a>
-          <a
-            href={
-              this.props?.sharedBasicInfo?.social?.find(
-                (network) => network.name === "github"
-              )?.url
-            }
-            rel="noopener noreferrer"
-            target="_blank"
-          >
-            <Lottie loop animationData={gitHubIcon} play className="socicon" />
-          </a>
-        </div>
-        <footer>
-          <div className="container">
-            <div className="row">
-              <div className="col social-links">{networks}</div>
-              <div className="col">
-                {/* Language selection will be disabled for now */}
-                <div
-                  onClick={() =>
-                    this.props.applyPickedLanguage(
-                      window.$primaryLanguage,
-                      window.$secondaryLanguageIconId
-                    )
-                  }
-                  style={{ display: "none" }}
-                >
-                  <span
-                    className="iconify language-icon mr-5"
-                    data-icon="twemoji-flag-for-flag-united-kingdom"
-                    data-inline="false"
-                    id={window.$primaryLanguageIconId}
-                  ></span>
-                </div>
-                <div
-                  onClick={() =>
-                    this.props.applyPickedLanguage(
-                      window.$secondaryLanguage,
-                      window.$primaryLanguageIconId
-                    )
-                  }
-                  style={{ display: "none" }}
-                >
-                  <span
-                    className="iconify language-icon"
-                    data-icon="twemoji-flag-for-flag-poland"
-                    data-inline="false"
-                    id={window.$secondaryLanguageIconId}
-                  ></span>
-                </div>
-              </div>
-              <div className="col">
-                <div className="copyright py-4 text-center">
-                  <div className="container">
-                    <small>
-                      &copy;
-                      {` 2023 ${this.props.sharedBasicInfo?.name || ""}`}
-                    </small>
-                  </div>
-                </div>
-              </div>
+      <Container as={"footer"} fluid>
+        <Row>
+          <Col className="v-flex text-start m-3">
+            <Row>
+              <Col>&copy; 2023</Col>
+            </Row>
+            <Row>
+              <Col>{this.props.sharedBasicInfo?.name || ""}</Col>
+            </Row>
+          </Col>
+          <Col>
+            <div id="socials" className="socials">
+              <a
+                href={
+                  this.props?.sharedBasicInfo?.social?.find(
+                    (network) => network.name === "linkedin"
+                  )?.url
+                }
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Lottie
+                  loop={animationsActive}
+                  animationData={linkedInIcon}
+                  play={animationsActive}
+                  className="socicon"
+                />
+              </a>
+              <a
+                href={
+                  this.props?.sharedBasicInfo?.social?.find(
+                    (network) => network.name === "github"
+                  )?.url
+                }
+                rel="noopener noreferrer"
+                target="_blank"
+              >
+                <Lottie
+                  loop={animationsActive}
+                  animationData={gitHubIcon}
+                  play={animationsActive}
+                  className="socicon"
+                />
+              </a>
             </div>
-          </div>
-        </footer>
-      </>
+          </Col>
+          <Col>
+            {/* Language selection will remain disabled */}
+            <div
+              onClick={() =>
+                this.props.applyPickedLanguage(
+                  window.$primaryLanguage,
+                  window.$secondaryLanguageIconId
+                )
+              }
+              style={{ display: "none" }}
+            >
+              <span
+                className="iconify language-icon mr-5"
+                data-icon="twemoji-flag-for-flag-united-kingdom"
+                data-inline="false"
+                id={window.$primaryLanguageIconId}
+              ></span>
+            </div>
+            <div
+              onClick={() =>
+                this.props.applyPickedLanguage(
+                  window.$secondaryLanguage,
+                  window.$primaryLanguageIconId
+                )
+              }
+              style={{ display: "none" }}
+            >
+              <span
+                className="iconify language-icon"
+                data-icon="twemoji-flag-for-flag-poland"
+                data-inline="false"
+                id={window.$secondaryLanguageIconId}
+              ></span>
+            </div>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
